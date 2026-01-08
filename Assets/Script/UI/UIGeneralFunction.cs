c25f8dfc3fa337645caef45a4e7fc6d4f32f0cd1
WUYUGE <wuyuge66699@163.com>
2026-01-08 10:22:33 +0800

    完善历史对话

--- Assets/Script/UI/UIGeneralFunction.cs
+++ Assets/Script/UI/UIGeneralFunction.cs
@@ -1,13 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UIGeneralFunction : MonoBehaviour
 {
     public GameObject orderObject;
+    public TalkSystem talkSystem1, talkSystem2;
 
     public void SetState()
     {
+        if (talkSystem1 is not null) talkSystem1.showText.CanShowText = orderObject.activeSelf;
+        if (talkSystem2 is not null) talkSystem2.showText.CanShowText = orderObject.activeSelf;
         orderObject.SetActive(!orderObject.activeSelf);
     }
     
